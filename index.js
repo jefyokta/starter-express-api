ac8e3548b2ef59d2e42d30747d601165a6a5ef59
Jepi Oktamipa <jefyokta50@gmail.com>
2024-04-18 06:59:37 +0700

    memperbaiki error api ai

--- index.js
+++ index.js
@@ -5,21 +5,27 @@ const app = express()
 require('dotenv').config()
 
 const genAi = new GoogleGenerativeAI(process.env.APIKEY)
+const run = async (input) => {
+    const model = genAi.getGenerativeModel({ model: "gemini-pro" });
+    const prompt = input
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+    return text
+}
 
-app.get('/gemini', (req, res) => {
+app.get('/gemini', async (req, res) => {
     const input = req.query.t
-    const run = async () => {
-        const model = genAi.getGenerativeModel({ model: "gemini-pro" });
-        const prompt = input
-        const result = await model.generateContent(prompt);
-        const response = await result.response;
-        const text = response.text();
-        console.log(text);
-        res.json(text)
-    }
+    const response = await run(input)
+    res.json({
+        response,
+        prompt: input
+    })
+
 })
 
-app.get('/' ,(req,res) => res.json('ok'))
+app.get('/', (req, res) => res.json('ok'))
 
 
 app.listen(process.env.PORT || 3000)
